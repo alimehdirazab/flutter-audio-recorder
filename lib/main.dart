90372e7cdeceb9595df23f5e9e2071b2a0b57d68
alimehdirazab <alimehdirazab@gmail.com>
2023-12-19 14:04:27 +0500

    i have worked on audio record and play that audio for my practice

---
+++ lib/main.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:record/record.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Audio Recored'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Record audioRecord;
+  late AudioPlayer audioPlayer;
+  bool isRecording = false;
+  String audioPath = '';
+
+  @override
+  void initState() {
+    audioPlayer = AudioPlayer();
+    audioRecord = Record();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    audioRecord.dispose();
+    audioPlayer.dispose();
+    super.dispose();
+  }
+
+// start recording function
+  Future<void> startRecording() async {
+    try {
+      if (await audioRecord.hasPermission()) {
+        await audioRecord.start();
+        setState(() {
+          isRecording = true;
+        });
+      }
+    } catch (e) {
+      print('Start Recording Error $e');
+    }
+  }
+
+//stop recodring function
+
+  Future<void> stopRecording() async {
+    try {
+      String? path = await audioRecord.stop();
+      setState(() {
+        isRecording = false;
+        audioPath = path!;
+      });
+    } catch (e) {
+      print('Stop Recording Error  $e');
+    }
+  }
+
+  Future<void> playRecording() async {
+    try {
+      Source sourceUrl = UrlSource(audioPath);
+      await audioPlayer.play(sourceUrl);
+    } catch (e) {
+      print('Play Recording Error $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (isRecording)
+              const Text(
+                'Audio Recoding........',
+                style: TextStyle(fontSize: 20),
+              ),
+            ElevatedButton(
+              onPressed: isRecording ? stopRecording : startRecording,
+              child: isRecording
+                  ? const Text('Stop Recording')
+                  : const Text('Start Recording'),
+            ),
+            const SizedBox(height: 20),
+            if (!isRecording && audioPath != null)
+              ElevatedButton(
+                onPressed: playRecording,
+                child: const Text('Play Recording'),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
